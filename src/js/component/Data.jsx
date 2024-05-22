bc7d369e330aad101ea472d351144af533b3acb4
AngelikaWebDev <110480648+AngelikaWebDev@users.noreply.github.com>
2024-05-22 14:38:26 +0000

    maqueta formulario

---
+++ src/js/component/Data.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Data = () => {
+
+
+    return(
+        <form>
+            <div className="mb-3">
+                <label className="form-label">Nombre</label>
+                <input type="text" className="form-control" id="name" aria-describedby="name"/>
+            </div>
+            <div className="mb-3">
+            <label className="form-label">Fecha de Nacimiento</label>
+                <div className="row">
+                    <div className="col">
+                        <label className="form-label">Día</label>
+                        <input type="number" className="form-control" id="day" aria-describedby="day"/>
+                    </div>
+                    <div className="col">
+                        <label className="form-label">Mes</label>
+                        <input type="number" className="form-control" id="month" aria-describedby="day"/>
+                    </div>
+                    <div className="col">
+                        <label className="form-label">Año</label>
+                        <input type="number" className="form-control" id="year" aria-describedby="year"/>
+                    </div>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default Data;
